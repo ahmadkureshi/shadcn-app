05c4db006ed4d7e629d6eb89c245fb84f5d84eb7
Ammar Asjad Raja <ammar.raja1@gmail.com>
2024-05-09 14:39:36 +0500

    shadcn basics

---
+++ src/app/accord/page.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+
+const Accord = () => {
+  return (
+    <div className="flex justify-center items-center h-full">
+      <main className="w-[500px] p-4">
+        <Accordion type="single" collapsible>
+          <AccordionItem value="item-1">
+            <AccordionTrigger>Is it accessible?</AccordionTrigger>
+            <AccordionContent>
+              Yes. It adheres to the WAI-ARIA design pattern.
+            </AccordionContent>
+          </AccordionItem>
+        </Accordion>
+      </main>
+    </div>
+  );
+};
+
+export default Accord;
---
+++ src/app/home/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <main className="min-h-screen flex justify-center items-center flex-col">
+      <div>HomePage</div>
+      <Button variant="chai" size='sm'>New Button</Button>
+    </main>
+  );
+};
+
+export default HomePage;
